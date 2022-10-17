b6125cb9560d9189fbdfd9a7aff57c09366ecddc
HoaiAnHo <hoaianho3100@gmail.com>
2022-10-16 23:27:16 -0700

    adding python files
    not complete yet, but a start

---
+++ pcap_analyze.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import dpkt
+import datetime
+import socket
+from dpkt.compat import compat_ord
+
+# count number of devices connected to hotspot
+
+# count number of packets sent by each device
+
+# count number of packets received by each device
+
+# find any endpoints where more than one device sends out a network packet to it
+# print out the IP addresses of these endpoints
+
+# count number of each application layer protocol used by each device
+
+# find the total time elapsed in the PCAP file (in minutes)
+
+if __name__ == '__main__':
+    print("hi")
